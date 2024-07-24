7112dbf32b8abaa92574bf9b85ae8511b6826479
Carlos Alzate <158311146+cdalzate91@users.noreply.github.com>
2024-07-24 22:18:31 +0000

    Modificación del app.js

--- src/app.js
+++ src/app.js
@@ -10,11 +10,14 @@ window.onload = function() {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net"];
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        for (let l = 0; l < extension.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + extension[l]);
+        }
       }
     }
   }
